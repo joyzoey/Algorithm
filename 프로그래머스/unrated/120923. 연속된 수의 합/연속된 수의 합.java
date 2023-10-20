4d109749930a71dbba1685c2b32fed49e9801ed9
Nahye <143480649+joyzoey@users.noreply.github.com>
2023-10-20 14:00:02 +0900

    [level unrated] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120923. 연속된 수의 합/연속된 수의 합.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int start;
+        if(num%2==0)
+        	start=(total/num)-(num/2-1);
+        else
+        	start=(total/num)-(num/2);
+        
+        for (int i = 0; i < num; i++) {
+            answer[i] = start;
+            start++;
+        }
+        return answer;
+    }
+}
